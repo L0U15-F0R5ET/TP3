b6ef344e337ceb56d57a604445f152a8efe45f0c
forgetl <forgetl@educ.college-montreal.qc.ca>
2025-10-15 16:15:59 -0400

    TP3 avancement

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ import random
 
 niveau_de_vie = 20
 play_game = True
+showed_rules = False
+force_adversaire = 0
 
 
 while play_game:
-    while showed_rules:
+    if not showed_rules:
         force_adversaire = random.randint(1, 5)
     print(f"vous avez {niveau_de_vie} point de vie et votre adversaire en a {force_adversaire}")
     action = input("Que voulez vous faire?\n"
@@ -38,15 +40,10 @@ while play_game:
         showed_rules = True
         print("Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.\n"
               "Dans ce cas, le niveau de vie de l’usager est augmenté de la force de l’adversaire.\n"
-              "Une défaite a lieu lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.\n"
+              "Une défaite a lieu lorsque la valeur du dé lancé est inférieure ou égale à la force de l’adversaire.\n"
               "Dans ce cas, le niveau de vie de l’usager est diminué de la force de l’adversaire.\n"
               "La partie se termine lorsque les points de vie de l’usager tombent sous 0.\n"
-              "L’usager peut combattre ou éviter chaque adversaire, dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
+              "L’usager peut combattre ou éviter chaque adversaire, \n"
+              " dans le cas de l’évitement, il y a une pénalité de 1 point de vie.")
     elif action == "4":
         play_game = False
-
-
-
-
-
-
