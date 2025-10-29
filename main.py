7c84945765872ac3c8bb94c9310928503321f2fd
forgetl <forgetl@educ.college-montreal.qc.ca>
2025-10-29 10:40:37 -0400

    TP3 fin

--- main.py
+++ main.py
@@ -12,23 +12,58 @@ force_adversaire2 = 0
 nombre_de_monstres = 0
 puissance_attaque1 = 0
 puissance_attaque2 = 0
+monstres_battus = 0
+adversaire_boss = 0
+
 while play_game:
     if not showed_rules:
         force_adversaire1 = random.randint(1, 5)
         force_adversaire2 = random.randint(1, 3)
         nombre_de_monstres = random.randint(1, 2)
-
+        adversaire_boss = random.randint(3, 5)
+    if monstres_battus == 3:
+        nombre_de_monstres = 0
+        print(f"un boss apparait devant vous! il a {adversaire_boss} points de vies")
+    if monstres_battus == 4:
+        nombre_de_monstres = 0
+        print(f"un boss apparait devant vous! il a {adversaire_boss} points de vies")
     if nombre_de_monstres == 1:
         print(f"vous avez {niveau_de_vie} point de vie et votre adversaire en a {force_adversaire1}")
     elif nombre_de_monstres == 2:
         print(f"vous avez {niveau_de_vie} points de vies, deux adversaires se trouvent dans la prochaine piece \n"
               f"le premier a {force_adversaire1} point de vies et le deuxieme en a {force_adversaire2}")
+
     action = input("Que voulez vous faire?\n"
                    "1- Combattre cet adversaire\n"
                    "2- Contournez cet adversaire et aller ouvrir une autre porte (-1 pv)\n"
                    "3- Afficher les regles du jeu \n"            
                    "4- Quitter la partie")
     if action == "1":
+
+        if monstres_battus == 3:
+            nombre_de_monstres = -1
+            puissance_attaque1 = random.randint(1, 6)
+            print(f"vous roulez un {puissance_attaque1} ")
+
+        if monstres_battus == 4:
+            nombre_de_monstres = -1
+            puissance_attaque1 = random.randint(1, 6)
+            print(f"vous roulez un {puissance_attaque1} pour attaquer le boss!")
+
+            if puissance_attaque1 > adversaire_boss:
+                print("vous avez battu un boss!")
+                niveau_de_vie += adversaire_boss
+                niveau_de_vie += adversaire_boss
+                print(f"vous avez maintenant {niveau_de_vie} point de vie!")
+
+            else:
+                print("le boss etait trop fort pour vous")
+                niveau_de_vie -= adversaire_boss
+                print(f"vous avez maintenant {niveau_de_vie} point de vie!")
+                monstres_battus = 0
+
+            monstres_battus = 0
+
         if nombre_de_monstres == 1:
             puissance_attaque1 = random.randint(1, 6)
             print(f"vous roulez un {puissance_attaque1} ")
@@ -37,32 +72,35 @@ while play_game:
                 print("vous venez a bout de votre adversaire!")
                 niveau_de_vie += force_adversaire1
                 print(f"vous avez maintenant {niveau_de_vie} point de vie!")
+                monstres_battus += 1
+
+            else:
+                print("vous n avez pas ete a la hauteur")
+                niveau_de_vie -= force_adversaire1
+                print(f"vous avez maintenant {niveau_de_vie} point de vie!")
 
         elif nombre_de_monstres == 2:
             puissance_attaque1 = random.randint(1, 6)
             puissance_attaque2 = random.randint(1, 6)
             print(f"vous roulez un {puissance_attaque1} et un {puissance_attaque2}")
 
-            if puissance_attaque1 >= force_adversaire1:
+            if puissance_attaque1 > force_adversaire1:
                 print("vous etes venus a bout du monstre #1")
                 niveau_de_vie += force_adversaire1
+                monstres_battus += 1
 
-            if puissance_attaque2 >= force_adversaire2:
-                print("vous etes venus a bout du monstre #2")
-                niveau_de_vie += force_adversaire2
-
-            elif puissance_attaque1 <= force_adversaire1:
+            else:
                 print("le monstre #1 etait trop fort pour vous")
                 niveau_de_vie -= force_adversaire1
 
-            elif puissance_attaque2 <= force_adversaire2:
+            if puissance_attaque2 > force_adversaire2:
+                print("vous etes venus a bout du monstre #2")
+                niveau_de_vie += force_adversaire2
+                monstres_battus += 1
+
+            else:
                 print("le monstre #2 etait trop fort pour vous")
                 niveau_de_vie -= force_adversaire2
-
-        else:
-            print("vous n avez pas ete a la hauteur")
-            niveau_de_vie -= force_adversaire1
-            print(f"vous avez maintenant{niveau_de_vie} point de vie")
 
     elif action == "2":
         niveau_de_vie -= 1
